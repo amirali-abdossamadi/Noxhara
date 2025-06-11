04aa1e2005791e4247b6cc0b644b5ab4bafdc3be
Amirali Abdossamadi <amiraliabdossamadi0@gmail.com>
2025-06-11 16:45:11 +0330

    Initial commit for Noxhara theme

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Noxhara theme loaded');
+});
---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+// Enqueue styles and scripts
+function noxhara_enqueue_scripts() {
+    wp_enqueue_style( 'noxhara-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'noxhara-custom-style', get_template_directory_uri() . '/assets/css/style.css' );
+    wp_enqueue_script( 'noxhara-main-js', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), null, true );
+}
+add_action( 'wp_enqueue_scripts', 'noxhara_enqueue_scripts' );
+
+// Include custom function files
+require get_template_directory() . '/inc/custom-functions.php';
+require get_template_directory() . '/inc/template-tags.php';
+require get_template_directory() . '/inc/theme-hooks.php';
+
+// Theme setup
+function noxhara_setup() {
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'woocommerce' );
+    register_nav_menus( array(
+        'primary' => __( 'Primary Menu', 'noxhara' ),
+    ) );
+}
+add_action( 'after_setup_theme', 'noxhara_setup' );
